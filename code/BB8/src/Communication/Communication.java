360c5518ecf2f1a9c3f469115c725bb16e5ba8a7
EmiledeKoning <emiledekoning@ziggo.nl>
2016-05-26 20:49:02 +0200

    Main v0.2

--- code/BB8/src/Communication/Communication.java
+++ code/BB8/src/Communication/Communication.java
@@ -1,5 +1,25 @@
 package Communication;
 
 public class Communication {
-
+	private byte[] PI_ADDR;
+	private int PI_PORT;
+	private byte[] PYTHON_ADDR;
+	private int PYTHON_PORT;
+	
+	public void Communication(byte[] new_addr, int new_port)
+	{
+		this.PI_ADDR = new_addr;
+		this.PI_PORT = new_port;
+	}
+	public void Communication(byte[] new_pi_addr, int new_pi_port, byte[] new_python_addr, int new_python_port)
+	{
+		this.PI_ADDR = new_pi_addr;
+		this.PI_PORT = new_pi_port;
+		this.PYTHON_ADDR = new_python_addr;
+		this.PYTHON_PORT = new_python_port;
+	}
+	public boolean Message(char[] message){
+		
+		return false;
+	}
 }
--- code/BB8/src/Robot/Robot.java
+++ code/BB8/src/Robot/Robot.java
@@ -15,8 +15,18 @@ import Magnetometer.Magnetometer;
 import Temperature.Temperature;
 
 public class Robot {
+	private Gyro gyroSensor;
+	private Acceleration accelerationSensor;
+	private Altitude altitudeSensor;
+	private Magnetometer magnetometerSensor;
+	private Temperature temperatureSensor;
 
-	public static void main(String[] args) throws UnknownHostException {
+	public static void main(String[] args) throws UnknownHostException{
+		Robot BB8 = new Robot();
+		BB8.init();
+	}
+
+	private void init() throws UnknownHostException{
 		InetAddress addr = InetAddress.getLocalHost();
 		if (addr.getHostName() == "BB8_Head"){
 			Init_Head();
@@ -25,73 +35,62 @@ public class Robot {
 			Init_Body();
 		}
 	}
-
-	private static void Init_Head(){
+	private void Init_Head(){
 		Properties prop = new Properties();
-    	InputStream input = null;
-    	
-    	try {
+		InputStream input = null;
 
-    		input = new FileInputStream("/BB8/BB8.config");
+		try {
 
-    		// load a properties file
-    		prop.load(input);
+			input = new FileInputStream("/BB8/BB8.config");
 
-    		// get the adress value of each sensor from the config file and create a instance of each sensor
-    		Gyro GyroSensor = new Gyro(Byte.parseByte(prop.getProperty("Gyro_Addr")));
-    		Acceleration AccelerationSensor = new Acceleration(Byte.parseByte(prop.getProperty("Acc_Addr")));
-    		Altitude AltitudeSensor = new Altitude(Byte.parseByte(prop.getProperty("Alt_Addr")));
-    		// NOT A I2C thing!! Echo EchoSensor = new Echo(Byte.parseByte(prop.getProperty("Echo_Addr")));
-    		Magnetometer MagnetometerSensor = new Magnetometer(Byte.parseByte(prop.getProperty("Magn_Addr")));
-    		Temperature TemperatureSensor = new Temperature(Byte.parseByte(prop.getProperty("Temp_Addr")));
+			// load a properties file
+			prop.load(input);
 
-    		System.out.println(prop.getProperty(""));
+			// get the address value of each sensor from the config file and create a instance of each sensor
+			this.gyroSensor = new Gyro(Byte.parseByte(prop.getProperty("Gyro_Addr")));
+			this.accelerationSensor = new Acceleration(Byte.parseByte(prop.getProperty("Acc_Addr")));
+			this.altitudeSensor = new Altitude(Byte.parseByte(prop.getProperty("Alt_Addr")));
+			// NOT A I2C thing!! Echo EchoSensor = new Echo(Byte.parseByte(prop.getProperty("Echo_Addr")));
+			this.magnetometerSensor = new Magnetometer(Byte.parseByte(prop.getProperty("Magn_Addr")));
+			this.temperatureSensor = new Temperature(Byte.parseByte(prop.getProperty("Temp_Addr")));
 
-    	} catch (IOException ex) {
-    		ex.printStackTrace();
-    	} finally {
-    		if (input != null) {
-    			try {
-    				input.close();
-    			} catch (IOException e) {
-    				e.printStackTrace();
-    			}
-    		}
-    	}
-		
+			System.out.println(prop.getProperty(""));
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
 	}
 	private static void Init_Body(){
-		Properties prop = new Properties();
-    	InputStream input = null;
-    	
-    	try {
-
-    		input = new FileInputStream("/BB8/BB8.config");
-
-    		// load a properties file
-    		prop.load(input);
-
-    		// get the adress value of each motor(hat) from the config file and create a instance of each motor
-    		
-
-    		System.out.println(prop.getProperty(""));
-
-    	} catch (IOException ex) {
-    		ex.printStackTrace();
-    	} finally {
-    		if (input != null) {
-    			try {
-    				input.close();
-    			} catch (IOException e) {
-    				e.printStackTrace();
-    			}
-    		}
-    	}
 	}
+	
 	public static void Shutdown() throws IOException{
 		Runtime.getRuntime().exec("sudo shutdown now");
 	}
 	public static void Reboot() throws IOException{
 		Runtime.getRuntime().exec("sudo reboot");
+	}
+	public Gyro get_gyroSensor(){
+		return this.gyroSensor;
+	}
+	public Acceleration get_accelerationSensor(){
+		return this.accelerationSensor;
+	}
+	public Altitude get_altitudeSensor(){
+		return this.altitudeSensor;
+	}
+	public Magnetometer get_magnetometerSensor(){
+		return this.magnetometerSensor;
+	}
+	public Temperature get_temperatureSensor(){
+		return this.temperatureSensor;
 	}
 }
